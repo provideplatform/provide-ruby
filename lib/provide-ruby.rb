264b06de599fb8479869364b789e447c76eaa175
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2015-06-25 09:26:37 -0400

    Populate email used for provider contact for testing

--- lib/provide-ruby.rb
+++ lib/provide-ruby.rb
@@ -162,7 +162,7 @@ module Provide
         state: nil,
         zip: nil,
         time_zone_id: 'Eastern Time (US & Canada)', # FIXME
-        email: nil,
+        email: "kyle+provider#{payload[:contractor_name].downcase.split(/\s+/)[0].trim}@unmarkedconsulting.com",
         phone: nil,
         mobile: nil,
       }
