94a55f44ccce1449a0dd5dadee09967f2ecd9683
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-10-17 03:25:35 -0400

    Remove dead tests

--- spec/provide-ruby/models/model_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Provide::Model do
-
-end
--- spec/provide-ruby/models/product_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Provide::Product do
-  it 'exposes :find' do
-    expect(Provide::Product.find(1).code).not_to be nil
-  end
-end
