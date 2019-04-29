0a15013bd7bf8817f1dc68a4be2c2d910acfd5de
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2019-04-29 18:31:20 -0400

    Add bookie service

--- lib/provide-ruby/services/bookie.rb
+++ lib/provide-ruby/services/bookie.rb
@@ -20,22 +20,55 @@ module Provide
         parse client.delete "payment_methods/#{payment_method_id}"
       end
 
+      def create_channel(params)
+        parse client.post 'channels', params
+      end
+  
+      def update_channel(application_id, params)
+        parse client.put "channels/#{application_id}", params
+      end
+  
+      def channels
+        parse client.get 'channels'
+      end
+  
+      def network
+        parse client.get 'network'
+      end
+
+      def network_invite(params)
+        parse client.post 'network', params
+      end
+      
       private
 
       def client
         @client ||= begin
+<<<<<<< HEAD
         Provide::ApiClient.new(@scheme, @host, 'api/v1/', @token)
+=======
+          Provide::ApiClient.new(@scheme, @host, 'api/v1/', @token)
+>>>>>>> Add bookie service
         end
       end
 
       def parse(response)
         begin
+<<<<<<< HEAD
         body = response.code == 204 ? nil : JSON.parse(response.body)
         return response.code, response.headers, body
         rescue
         raise Exception.new({
             :code => response.code,
         })
+=======
+          body = response.code == 204 ? nil : JSON.parse(response.body)
+          return response.code, body
+        rescue
+          raise Exception.new({
+            :code => response.code,
+          })
+>>>>>>> Add bookie service
         end
       end
     end
