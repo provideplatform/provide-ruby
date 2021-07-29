25ada74ea008677224b85f602b91a110c7aa592d
Alex Dunne <alex12dunne@gmail.com>
2021-07-29 02:16:26 -0400

    require vault in provide-ruby.rb

--- lib/provide-ruby.rb
+++ lib/provide-ruby.rb
@@ -8,6 +8,7 @@ require 'provide-ruby/services/bookie'
 require 'provide-ruby/services/nchain'
 require 'provide-ruby/services/ident'
 require 'provide-ruby/services/tourguide'
+require 'provide-ruby/services/vault'
 
 # Provide is the top-level module provided by provide-ruby.
 module Provide
